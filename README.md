d9a285dd0b27af4ebe94f25d47509558890975cc
Korab Hoxha <korab-h@hotmail.com>
2016-12-24 15:58:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Development
+
+### Run app
+
+1. Clone repo ```git clone https://github.com/.git```
+2. Change to repo ```cd timings-app```
+3. ```npm install && npm start```
+
+### Build
+
+Builds app binaries for OS X, Linux, and Windows.
+
+```npm run build```
---
+++ src/app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const electron = require('electron')
+const app = electron.app
+const menubar = require('menubar')
+const config = require('./../package.json')
+const path = require('path')
+
+
+let autoLaunch = true
+let iconSetting = 'auto'
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('will-quit', function () {
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll()
+})
+
+const mb = menubar({
+  index: path.join('file://', __dirname, '/main/index.html'),
+  icon: path.join(__dirname, '/../assets/IconTemplate.png'),
+  width: 280,
+  height: 480,
+  // resizable: true,
+  showDockIcon: false,
+  preloadWindow: true
+})
+
