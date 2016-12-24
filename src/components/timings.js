c66bfdc4bbdaec4f94a552f43ee8db439dced882
Korab Hoxha <korab-h@hotmail.com>
2016-12-24 16:46:30 +0100

    Refresh timings

--- src/components/timings.js
+++ src/components/timings.js
@@ -2,6 +2,8 @@
 
 const superagent = require('superagent')
 const jQuery = require('jquery')
+const utils = require('./../utilities/utils')
+const store = require('./../utilities/store')
 
 const config = require('./../main/config.json')
 
@@ -14,7 +16,25 @@ const getTimingsDaily = function (url, option, callback) {
         utils.showErrorMessage('Failure during data fetching')
         console.log(err)
       } else {
-      
+        const wdata = store.getWdata()
+        wdata[option] = res.body
+        store.setWdata(wdata)
+        if (wdata[option].cod !== 404) {
+          // wdata localStorage
+        }
       }
     }
 }
+
+const refreshInfo = function () {
+  setInterval(function () {
+    refreshTimings()
+    console.log('refresh timings')
+  }, config.start.interval
+}
+
+const refreshTimings = function () {
+}
+
+exports.getTimingsDaily = getTimingsDaily
+exports.refreshInfo = refreshInfo
--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -3,9 +3,21 @@ const config = require('./../main/config.json')
 
 const jQuery = require('jquery')
 
+let wdata = {}
+
+const getWdata = function () {
+  return wdata
+}
+
+const setWdata = function (data) {
+  wdata = data
+}
+
 const showErrorMessage = function (message) {
   hideAll()
-  jQuery('#main .content .time-note').html(message)
+  jQuery('#main .content .note').html(message)
 }
 
 exports.showErrorMessage = showErrorMessage
+exports.getWdata = getWdata
+exports.setWdata = setWdata
